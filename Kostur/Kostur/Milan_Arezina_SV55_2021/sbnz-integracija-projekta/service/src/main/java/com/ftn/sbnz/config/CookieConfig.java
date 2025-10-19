9e6c8c9de6a3af9c36fc9b666d02ea58c24e3161
Milan Arezina <arezinamilann@gmail.com>
2025-10-19 22:36:26 +0200

    adjust cookie settings for Render

---
+++ Kostur/Kostur/Milan_Arezina_SV55_2021/sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/config/CookieConfig.java
@@ -0,0 +1,13 @@
+package com.ftn.sbnz.config;
+
+import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CookieConfig {
+    @Bean
+    public CookieSameSiteSupplier applicationCookieSameSiteSupplier() {
+        return CookieSameSiteSupplier.ofNone();
+    }
+}
